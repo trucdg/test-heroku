8f5361e5ac4fbdecf3f75c41f8e7184e493259f9
Truc Duong <trucdg@bu.edu>
2024-10-10 14:45:57 -0400

    add STATIC_ROOT to support staticfiles during deployment

--- cs412/settings.py
+++ cs412/settings.py
@@ -124,6 +124,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),  # Project-level static directory
 ]
 
+# Directory where static files will be collected during deployment (for production)
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
