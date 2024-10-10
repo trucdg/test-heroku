12c47b8b92a9bf4b67bca87247557ea7642131e9
Truc Duong <trucdg@bu.edu>
2024-10-10 14:59:26 -0400

    add static file path to project urls.py

--- cs412/settings.py
+++ cs412/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",  # Add whitenoise to deploy static files
+    # "whitenoise.middleware.WhiteNoiseMiddleware",  # Add whitenoise to deploy static files
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
--- cs412/urls.py
+++ cs412/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     # Include the URLs for the 'quotes' app in the main project URL patterns.
     path("quotes/", include("quotes.urls")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
